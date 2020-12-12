3075b69f71fc1a389d37ea40c35a6678c214b515
Solovyev <Evgenuy1605@yandex.ru>
2020-12-12 15:50:39 +0300

    Add shared test

---
+++ AnthillDI_DotNet.Tests/AnthillDISharedTests.cs
@@ -0,0 +1,33 @@
+﻿using AnthillDI_DotNet.Tests.TestClasses;
+using Xunit;
+
+namespace AnthillDI_DotNet.Tests
+{
+    public class AnthillDISharedTests
+    {
+        [Fact]
+        public void TestInjectFuncInitialization_Ok()
+        {
+            AHDI di = new AHDI();
+
+            di.SetSingletonObject<ClassWithoutParamsInConstructor>();
+            di.SetRequestedObject((context)
+                => new ClassWithMultipleParameterInConstructor(
+                    context.GetObject<ClassWithoutParamsInConstructor>(),
+                    new ClassWithSingleParameterInConstructor(context.GetObject<ClassWithoutParamsInConstructor>())));
+
+            ClassWithMultipleParameterInConstructor type = di.GetObject<ClassWithMultipleParameterInConstructor>();
+
+            Assert.Equal(1, type.WithoutParam.TestNum);
+            Assert.Equal(2, type.WithoutParam.TestNum2);
+            Assert.Equal("1", type.WithoutParam.TestStr);
+            Assert.Equal("2", type.WithoutParam.TestStr2);
+            Assert.Equal(1, type.WithSingleParam.TestInjectedClass.TestNum);
+            Assert.Equal(2, type.WithSingleParam.TestInjectedClass.TestNum2);
+            Assert.Equal("1", type.WithSingleParam.TestInjectedClass.TestStr);
+            Assert.Equal("2", type.WithSingleParam.TestInjectedClass.TestStr2);
+
+            Assert.Equal(type.WithoutParam, type.WithSingleParam.TestInjectedClass);
+        }
+    }
+}
